594e7a13546d7189a10713125d620a2433c8813f
Adrian Américo <1fnadrian1@gmail.com>
2024-07-13 00:45:42 -0300

    refactor: update the trip controller

--- src/main/java/com/example/planner/trip/TripController.java
+++ src/main/java/com/example/planner/trip/TripController.java
@@ -1,37 +1,81 @@
 package com.example.planner.trip;
 
-import com.example.planner.participant.ParticipantService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.planner.participant.ParticipantService;
 
 @RestController
 @RequestMapping("/trips")
 public class TripController {
-    @Autowired
-    private ParticipantService participantService;
+	@Autowired
+	private ParticipantService participantService;
 
-    @Autowired
-    private TripRepository repository;
+	@Autowired
+	private TripRepository repository;
 
-    @PostMapping
-    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
-        Trip newTrip = new Trip(payload);
+	@PostMapping
+	public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
+		Trip newTrip = new Trip(payload);
 
-        this.repository.save(newTrip);
+		this.repository.save(newTrip);
 
-        this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
+		this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip);
 
-        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
-    }
+		return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
-        Optional<Trip> trip = this.repository.findById(id);
+	@GetMapping("/{id}")
+	public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id) {
+		Optional<Trip> trip = this.repository.findById(id);
 
-        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
+		return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload) {
+		Optional<Trip> trip = this.repository.findById(id);
+
+		if (trip.isPresent()) {
+			Trip rowTrip = trip.get();
+
+			rowTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+			rowTrip.setStartsAt(LocalDateTime.parse((payload.starts_at()), DateTimeFormatter.ISO_DATE_TIME));
+			rowTrip.setDestination(payload.destination());
+
+			this.repository.save(rowTrip);
+			return ResponseEntity.ok(rowTrip);
+		}
+
+		return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/{id}/confirm")
+	public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id, @RequestBody TripRequestPayload payload) {
+		Optional<Trip> trip = this.repository.findById(id);
+
+		if (trip.isPresent()) {
+			Trip rowTrip = trip.get();
+
+			rowTrip.setIsConfirmed(true);
+
+			this.repository.save(rowTrip);
+			this.participantService.triggerConfirmationEmailToParticipants(id);
+			return ResponseEntity.ok(rowTrip);
+		}
+
+		return ResponseEntity.notFound().build();
+	}
 }
