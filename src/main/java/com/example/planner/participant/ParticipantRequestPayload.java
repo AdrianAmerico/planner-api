b963e310b08de92d0dad530ac75c505b7080cd6f
Adrian Américo <1fnadrian1@gmail.com>
2024-07-13 00:45:30 -0300

    feat: add participant

---
+++ src/main/java/com/example/planner/participant/ParticipantRequestPayload.java
@@ -0,0 +1,6 @@
+
+package com.example.planner.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+
+}
--- src/main/java/com/example/planner/participant/ParticipantService.java
+++ src/main/java/com/example/planner/participant/ParticipantService.java
@@ -1,15 +1,26 @@
 package com.example.planner.participant;
 
-import com.example.planner.trip.TripRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.planner.trip.Trip;
+
 @Service
 public class ParticipantService {
-    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID id) {
+    @Autowired
+    private ParticipantRepository repository;
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip) {
+
+        List<Participant> participants = participantsToInvite.stream()
+                .map(email -> new Participant(email, trip)).toList();
+
+        this.repository.saveAll(participants);
+
+        System.out.println(participants.get(0).getId());
     }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId) {
